cf972ad726c006499247c3623ea9d4c42973706f
itisioslab <itisioslab@Mac-mini-administrator-4.local>
2014-08-25 13:00:08 +0400

    ничего не изменил

---
+++ SSNow/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  SSNow
+//
+//  Created by itisioslab on 25.08.14.
+//  Copyright (c) 2014 kpfu.itisioslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
